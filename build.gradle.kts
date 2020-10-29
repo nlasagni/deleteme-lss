644d123f525de921e6c0c1c3be13f10ede503faf
Nicola Lasagni <nicola.lasagni@gmail.com>
2020-10-29 14:43:42 +0100

    Add signing and delivery

--- build.gradle.kts
+++ build.gradle.kts
@@ -121,3 +121,11 @@ publishing {
         }
     }
 }
+
+if (System.getenv("CI") == "true") {
+    signing {
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
+}
